71753afaa53836db02fe3d7285e4714345262081
nair-bharat <bharatmnair246@gmail.com>
2022-08-11 00:01:04 +0530

    removing the root folder

---
+++ index.js
@@ -0,0 +1,22 @@
+function switchVisible() {
+    if (document.getElementById('rating-state__container__div1')) {
+
+        if (document.getElementById('rating-state__container__div1').style.display == 'none') {
+            document.getElementById('rating-state__container__div1').style.display = 'block';
+            document.getElementById('rating-state__container__div2').style.display = 'none';
+        }
+        else {
+            var ele = document.getElementsByName('radioRating');
+            
+                for (i = 0; i < ele.length; i++) {
+                    if (ele[i].checked)
+                        document.getElementById("result").innerHTML
+                            = "You selected " + ele[i].value + " out of 5";
+                }
+            document.getElementById('rating-state__container__div1').style.display = 'none';
+            document.getElementById('rating-state__container__div2').style.display = 'flex';
+        }
+    }
+}
+
+
