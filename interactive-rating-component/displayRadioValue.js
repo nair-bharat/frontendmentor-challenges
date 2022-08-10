d4e9b43fded31d06951393b164cedb3863beae96
nair-bharat <bharatmnair246@gmail.com>
2022-08-10 23:11:14 +0530

    completed solution

--- interactive-rating-component/displayRadioValue.js
+++
@@ -1,9 +0,0 @@
-function displayRadioValue() {
-    var ele = document.getElementsByName('radioRating');
-
-    for (i = 0; i < ele.length; i++) {
-        if (ele[i].checked)
-            document.getElementById("result").innerHTML
-                = "You selected " + ele[i].value + " out of 5";
-    }
-}
